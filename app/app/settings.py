7ac73ee2805b93a68eea4158b21b28549984dfd9
IsmaelBortoluzzi <ismael.bortoluzzi77@gmail.com>
2022-05-24 10:56:07 -0300

    configuring the database

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-c&jm_sg(e_i36fd1^2pw4*c(b$f0r7+(gk0p1qg*f*im3!xnb(
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.app.mail.backends.console.EmailBackend'
 
 
 # Application definition
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,8 +76,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bar',
+        'HOST': 'localhost',
+        'PORT': 7438,
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
     }
 }
 
@@ -120,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (BASE_DIR / 'templates/static',)
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
