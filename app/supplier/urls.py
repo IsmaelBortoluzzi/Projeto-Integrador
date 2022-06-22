ac8a6e16a07e5f466709ac7b93d1d8c3fb04a47a
Ismael <ismael.bortoluzzi77@gmail.com>
2022-06-22 15:39:58 -0300

    Criando list-supplier supplier.

--- app/supplier/urls.py
+++ app/supplier/urls.py
@@ -4,6 +4,7 @@ from .models_select2queryset import SupplierAutocomplete
 from .views import *
 
 urlpatterns = [
+    path('list-supplier/', ListSupplier.as_view(), name='list-supplier'),
     path('detail-supplier/<int:pk>/', detail_supplier, name='detail-supplier'),
     # autocomplete
     path('supplier-autocomplete/', SupplierAutocomplete.as_view(), name='supplier-autocomplete')
--- app/supplier/views.py
+++ app/supplier/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from supplier.models import Supplier
+
+
+class ListSupplier(ListView):
+    model = Supplier
+    template_name = 'supplier/list_supplier.html'
+    paginate_by = 15
+    extra_context = dict()
+    context_object_name = 'suppliers'
+
+    def __init__(self, *args, **kwargs):
+        super(ListSupplier, self).__init__(*args, **kwargs)
+        self.codigo = self.nome = None
+
+    def get(self, request, *args, **kwargs):
+        self.codigo = self.request.GET.get('codigo', None)
+        self.nome = self.request.GET.get('nome', None)
+
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        query = """
+            SELECT SS.* FROM supplier_supplier SS
+        """
+
+        if self.codigo is not None:
+            query = query + " WHERE C.id = %s" % str(self.codigo)
+
+        elif self.nome is not None:
+            query = query + " WHERE PC.name = %s" % str(self.nome)
+
+        qs = Supplier.objects.raw(query)
+
+        return qs
 
 
 def detail_supplier(request, pk):
